28d0f32d50eecb7434e57f2b78cc1d9b05b9aa5e
zhectorSM <hsolaresm@gmail.com>
2020-03-31 14:08:43 -0700

    Added user table and er diagram

--- movieTheater.sql
+++ movieTheater.sql
@@ -2,80 +2,83 @@ create database movietheatre;
 use movietheatre;
 
 create table category (
-id_category int primary key auto_increment not null,
-category_name varchar (50) not null
+	id_category int primary key auto_increment not null,
+	category_name varchar (50) not null
 );
 
 
 create table movie (
-id_movie int primary key auto_increment not null,
-movie_name varchar(50) not null,
-id_category int not null,
-director varchar (50) not null,
-actor varchar (50) not null,
-movie_year int not null,
-runtime_minutes int not null,
-rate varchar (50) not null,
-FOREIGN KEY (id_category) REFERENCES category(id_category)
+	id_movie int primary key auto_increment not null,
+	movie_name varchar(50) not null,
+	id_category int not null,
+	director varchar (50) not null,
+	actor varchar (50) not null,
+	movie_year int not null,
+	runtime_minutes int not null,
+	rate varchar (50) not null,
+	FOREIGN KEY (id_category) REFERENCES category(id_category)
 );
 
 
 create table location(
-id_location int primary key auto_increment not null,
-location varchar (50),
-address varchar (50)
+	id_location int primary key auto_increment not null,
+	location varchar (50),
+	address varchar (50)
 );
 
 
 CREATE table auditorium(
-id_auditorium int PRIMARY KEY AUTO_INCREMENT not null,
-seat_total VARCHAR(50) NOT NULL,
-vip boolean NOT NULL,
-id_location int NOT NULL,
-FOREIGN KEY (id_location) REFERENCES location(id_location)
+	id_auditorium int PRIMARY KEY AUTO_INCREMENT not null,
+	seat_total VARCHAR(50) NOT NULL,
+	vip boolean NOT NULL,
+	id_location int NOT NULL,
+	FOREIGN KEY (id_location) REFERENCES location(id_location)
 );
 
 
 create table loyal_program (
-id_loyal_program int primary key auto_increment not null,
-discount float not null,
-minimum_points int not null
+	id_loyal_program int primary key auto_increment not null,
+	discount float not null,
+	minimum_points int not null
 );
 
 
 create table customer (
-id_customer int primary key auto_increment not null,
-first_name varchar (50) not null,
-last_name varchar (50) not null,
-gender varchar (1) null,
-email varchar (50) null,
-points int not null default 0
+	id_customer int primary key auto_increment not null,
+	first_name varchar (50) not null,
+	last_name varchar (50) not null,
+	gender varchar (1) null,
+	email varchar (50) null,
+	points int not null default 0
 );
 
 create table theatre_schedule (
-id_schedule int primary key auto_increment not null,
-showtime datetime not null,
-id_auditorium int not null,
-id_movie int not null,
-price float not null,
-seat int not null,
-FOREIGN KEY (id_movie) REFERENCES movie(id_movie),
-FOREIGN KEY (id_auditorium) REFERENCES auditorium(id_auditorium)
+	id_schedule int primary key auto_increment not null,
+	showtime datetime not null,
+	id_auditorium int not null,
+	id_movie int not null,
+	price float not null,
+	seat int not null,
+	FOREIGN KEY (id_movie) REFERENCES movie(id_movie),
+	FOREIGN KEY (id_auditorium) REFERENCES auditorium(id_auditorium)
 );
 
 create table ticket (
-id_ticket int primary key auto_increment not null,
-id_loyal_program float not null,
-id_customer int not null,
-id_schedule int not null,
-FOREIGN KEY (id_schedule) REFERENCES theatre_schedule(id_schedule),
-FOREIGN KEY (id_customer) REFERENCES customer(id_customer)
+	id_ticket int primary key auto_increment not null,
+	id_loyal_program float not null,
+	id_customer int not null,
+	id_schedule int not null,
+	FOREIGN KEY (id_schedule) REFERENCES theatre_schedule(id_schedule),
+	FOREIGN KEY (id_customer) REFERENCES customer(id_customer)
 );
 
 
-
-
-
+create table user (
+	id_user int primary key auto_increment not null,
+	name varchar (50),
+	pwd varchar (50),
+	role varchar (50) 
+);
 
 
 
