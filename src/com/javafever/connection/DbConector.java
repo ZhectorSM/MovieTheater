06c8b23d2a502def4b4fcc122bf2f9aa3eefb448
jcrf15 <59666296+jcrf15@users.noreply.github.com>
2020-04-01 11:36:11 -0700

    INPUT.NEXTLINE TRIM & COMMENTS

--- src/com/javafever/connection/DbConector.java
+++ src/com/javafever/connection/DbConector.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DbConector {
 
 	private static java.sql.Connection conn;
-	private static final String URL = "jdbc:mysql://localhost:3306/movietheatre?serverTimezone=PST";
-	private static final String USER = "zhectorSM";
-	private static final String PWD = "zhectorSM";
+	private static final String URL = "jdbc:mysql://localhost:8889/movietheatre?serverTimezone=PST";
+	private static final String USER = "root";
+	private static final String PWD = "Jcrf151681";
 
 	private DbConector() {
 	}
@@ -23,7 +23,7 @@ public class DbConector {
 				conn = DriverManager.getConnection(URL, USER, PWD);
 				if (conn != null) {
 
-					// System.out.println("Database connected succesfully.");
+					System.out.println("Database connected succesfully.");
 
 				}
 
