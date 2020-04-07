f51fb1d01881f537cc05bac6bf87036ec709805a
jcrf15 <59666296+jcrf15@users.noreply.github.com>
2020-04-06 22:05:44 -0700

    Showtime updated

--- src/com/javafever/connection/DbConector.java
+++ src/com/javafever/connection/DbConector.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DbConector {
 
 	private static java.sql.Connection conn;
-	private static final String URL = "jdbc:mysql://localhost:3306/movietheatre?serverTimezone=UTC";
-	private static final String USER = "zHectorSM";
-	private static final String PWD = "zHectorSM";
+	private static final String URL = "jdbc:mysql://localhost:8889/movietheatre?serverTimezone=UTC";
+	private static final String USER = "root";
+	private static final String PWD = "Jcrf151681";
 
 	private DbConector() {
 	}
