313eefa40651883218f15a41602dc9833ff0741a
zhectorSM <hsolaresm@gmail.com>
2020-04-06 22:57:40 -0700

    Scripts schedule , deleted not used files

--- preloaded_data.sql
+++ preloaded_data.sql
@@ -125,4 +125,31 @@ Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("20
 Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-11T15:30:00",10,6,"15.50");
 Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-11T19:00:00",11,8,"15.50");
 Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-11T23:00:00",12,9,"16.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-12T13:00:00",1,7,"10.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-12T16:00:00",2,7,"15.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-12T13:00:00",3,10,"10.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-12T16:00:00",4,10,"15.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-12T13:00:00",5,11,"10.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-12T16:00:00",6,11,"15.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-13T13:00:00",5,12,"10.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-13T23:59:00",6,12,"16.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-13T15:00:00",7,13,"15.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-13T23:59:00",8,13,"16.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-13T16:00:00",9,14,"15.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-13T23:59:00",10,14,"16.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-14T17:00:00",11,15,"15.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-14T23:59:00",12,15,"16.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-14T13:00:00",1,16,"10.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-14T23:59:00",1,16,"16.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-14T13:00:00",2,17,"10.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-14T21:00:00",3,17,"15.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-15T13:00:00",4,18,"10.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-15T21:00:00",5,18,"15.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-15T15:00:00",6,19,"15.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-15T23:59:00",7,19,"16.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-15T15:00:00",8,20,"15.50");
+Insert into theatre_schedule (showtime,id_auditorium,id_movie,price) values ("2020-04-15T23:59:00",9,20,"16.50");
+
+
+
 
